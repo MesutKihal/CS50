6d7a027bdb9a57c647d68719ef4acb1f234c5c75
Messaoud KIHAL <66731601+MesutKihal@users.noreply.github.com>
2021-10-14 21:44:57 +0100

    Update tic_tac_toe.c

--- tic_tac_toe.c
+++ tic_tac_toe.c
@@ -1,23 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void updateBoard(int arr[3][3]){
-    printf("  A  B  C\n");
-    for (int i = 0; i<3; i++){
-        printf("%i", i+1);
-        for (int j=0; j<3; j++){
-            if (arr[i][j] == 0){
-                printf(" - ");
-            }else if (arr[i][j] == 1){
-                printf(" X ");
-            }else if (arr[i][j] == 2){
-                printf(" O ");
-            }
-            
-        }
-        printf("\n");
-    }
-}
 
 
 int main()
@@ -26,29 +9,72 @@ int main()
     char cmd[2];
     int gameover = 0;
     char *turn = "X";
-    void checkBoard(int arr[3][3]){
-        // Function not finished
-        if (arr[0][0] ==  arr[0][1] ==  arr[0][2] != 0){
-            gameover = 1;
-        }else if (arr[1][0] == arr[1][1] == arr[1][2] != 0){
-            gameover = 1;
-        }else if (arr[2][0] == arr[2][1] == arr[2][2] != 0){
-            gameover = 1;
-        }else if (arr[0][0] == arr[1][0] == arr[2][0] != 0){
-            gameover = 1;
-        }else if (arr[0][1] == arr[1][1] == arr[2][1] != 0){
-            gameover = 1;
-        }else if (arr[0][2] == arr[1][2] == arr[2][2] != 0){
-            gameover = 1;
-        }else if (arr[0][0] == arr[1][1] == arr[2][2] != 0){
-            gameover = 1;
-        }else if (arr[0][2] == arr[1][1] == arr[2][0] != 0){
-            gameover = 1;
+    void updateBoard(int arr[3][3]){
+        printf("  a  b  c\n");
+        for (int i = 0; i<3; i++){
+            printf("%i", i+1);
+            for (int j=0; j<3; j++){
+                if (arr[i][j] == 0){
+                    printf(" - ");
+                }else if (arr[i][j] == 1){
+                    printf(" X ");
+                }else if (arr[i][j] == 2){
+                    printf(" O ");
+                }
+                
+            }
+            printf("\n");
+        }
+        if (gameover == 1){
+            if (turn == "X"){
+                printf("\nO's Victory!!\n");
+            }else if (turn == "O"){
+                printf("\nX's Victory!!\n");
+            }
         }
     }
+
+    void checkBoard(int arr[3][3]){
+        if (arr[0][0] == 1 &&  arr[0][1] == 1 && arr[0][2] == 1){
+            gameover = 1;
+        }else if (arr[1][0] == 1 && arr[1][1] == 1 && arr[1][2] == 1){
+            gameover = 1;
+        }else if (arr[2][0] == 1 && arr[2][1] == 1 && arr[2][2] == 1){
+            gameover = 1;
+        }else if (arr[0][0] == 1 && arr[1][0] == 1 && arr[2][0] == 1){
+            gameover = 1;
+        }else if (arr[0][1] == 1 && arr[1][1] == 1 && arr[2][1] == 1){
+            gameover = 1;
+        }else if (arr[0][2] == 1 && arr[1][2] == 1 && arr[2][2] == 1){
+            gameover = 1;
+        }else if (arr[0][0] == 1 && arr[1][1] == 1 && arr[2][2] == 1){
+            gameover = 1;
+        }else if (arr[0][2] == 1 && arr[1][1] == 1 && arr[2][0] == 1){
+            gameover = 1;
+        }else if (arr[0][0] == 2 &&  arr[0][1] == 2 && arr[0][2] == 1){
+            gameover = 1;
+        }else if (arr[1][0] == 2 && arr[1][1] == 2 && arr[1][2] == 2){
+            gameover = 1;
+        }else if (arr[2][0] == 2 && arr[2][1] == 2 && arr[2][2] == 2){
+            gameover = 1;
+        }else if (arr[0][0] == 2 && arr[1][0] == 2 && arr[2][0] == 2){
+            gameover = 1;
+        }else if (arr[0][1] == 2 && arr[1][1] == 2 && arr[2][1] == 2){
+            gameover = 1;
+        }else if (arr[0][2] == 2 && arr[1][2] == 2 && arr[2][2] == 2){
+            gameover = 1;
+        }else if (arr[0][0] == 2 && arr[1][1] == 2 && arr[2][2] == 2 ){
+            gameover = 1;
+        }else if (arr[0][2] == 2 && arr[1][1] == 2 && arr[2][0] == 2 ){
+            gameover = 1;
+        }
+
+    }
+    updateBoard(arr);
     do
     {
-        printf("Enter your command: ");
+        printf("\n%c's turn\n", *turn);
+        printf("Enter your coordinates: ");
         scanf("%s", cmd);
         
         int column = (int) cmd[0]-97;
@@ -72,6 +98,7 @@ int main()
                 turn = "X";
             }
         }
+        system("clear");
         checkBoard(arr);
         updateBoard(arr);
     }while (gameover == 0);
