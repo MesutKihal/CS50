a38b02224f574d2b22c7a885180e0265b48b0c03
Messaoud KIHAL <66731601+MesutKihal@users.noreply.github.com>
2021-10-14 12:53:48 +0100

    Update tic_tac_toe.c

--- tic_tac_toe.c
+++ tic_tac_toe.c
@@ -3,10 +3,17 @@
 
 void updateBoard(int arr[3][3]){
     printf("  A  B  C\n");
-    for (int j = 0; j<3; j++){
-        printf("%i", j+1);
-        for (int i=0; i<3; i++){
-            printf(" %i ", arr[i][j]);
+    for (int i = 0; i<3; i++){
+        printf("%i", i+1);
+        for (int j=0; j<3; j++){
+            if (arr[i][j] == 0){
+                printf(" - ");
+            }else if (arr[i][j] == 1){
+                printf(" X ");
+            }else if (arr[i][j] == 2){
+                printf(" O ");
+            }
+            
         }
         printf("\n");
     }
@@ -17,18 +24,24 @@ int main()
     int arr[3][3] = {{0, 0, 0}, {0, 0, 0}, {0, 0, 0}};
     char cmd[2];
     int gameover = 0;
-    char *turn = "A";
+    char *turn = "X";
     do
     {
         printf("Enter your command: ");
         scanf("%s", cmd);
-        if (turn == "A"){
-            int row = (int) cmd[0]-97;
-            char temp = cmd[1];
-            int column = (int) (temp - '0');
-            updateBoard(arr);
-            break;
+        
+        int column = (int) cmd[0]-97;
+        char temp = cmd[1];
+        int row = (int) (temp - '0')-1;
+
+        if (turn == "X"){
+            arr[row][column] = 1;
+            turn = "O";
+        }else if (turn == "O"){
+            arr[row][column] = 2;
+            turn = "X";
         }
+        updateBoard(arr);
     }while (gameover == 0);
     return 0;
 }
