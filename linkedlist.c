05e15142d5a675ef399ac77a2dde144d5f27aba9
Messaoud KIHAL <66731601+MesutKihal@users.noreply.github.com>
2021-10-26 15:10:19 +0100

    Update linkedlist.c

--- linkedlist.c
+++ linkedlist.c
@@ -1,6 +1,6 @@
+
 #include <stdio.h>
 #include <stdlib.h>
-
 
 int main()
 {
@@ -8,12 +8,34 @@ int main()
     {
         int value;
         struct node *next;
-    }
-    node;
-    
+    }node;
     node *a = malloc(sizeof(node));
     if (a != NULL){
+        a->value = 1;
         a->next = NULL;
     }
+    int append(struct node *a, int value){
+        node *b = malloc(sizeof(node));
+        b->value = value;
+        node *temp = a;
+        while (temp != NULL){
+            if(temp->next == NULL){
+                temp->next = b;
+                break;
+            }
+            temp = temp->next;
+        }
+    }
+    void printList(struct node *a){
+        node *temp = a;
+        while (temp != NULL){
+            printf("%d -> ",temp->value);
+            temp = temp->next;
+        }
+    }
+    append(a, 2);
+    append(a, 3);
+    append(a, 4);
+    printList(a);
+    return 0;
 }
-// 45min
