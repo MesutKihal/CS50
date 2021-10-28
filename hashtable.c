d40fc00961914b83f53d9b3adbb5351607e14ccf
Messaoud KIHAL <66731601+MesutKihal@users.noreply.github.com>
2021-10-28 15:55:53 +0100

    Update hashtable.c

--- hashtable.c
+++ hashtable.c
@@ -15,8 +15,33 @@ int main()
         char str[MAX_LEGNTH];
         struct node *next;
     }node;
-    // create hashtable
-    node *hashtable = malloc(sizeof(node)*10);
+    // linked list funtions
+    int append(struct node *a, char str[]){
+        node *b = malloc(sizeof(node));
+        int size = (int) strlen(str);
+        for (int i=0;i<size;i++){
+            b->str[i] = str[i];
+        }
+        node *temp = a;
+        while (temp != NULL){
+            if(temp->next == NULL){
+                temp->next = b;
+                break;
+            }
+            temp = temp->next;
+        }
+    }
+    void printList(struct node *a){
+        node *temp = a;
+        while (temp != NULL){
+            int size = strlen(temp->str);
+            for (int i=0;i<size;i++){
+                printf("%c",temp->str[i]);
+            }
+            printf("->");
+            temp = temp->next;
+        }
+    }
     // declare insert function
     void insert(struct node hashtable[], char str[]){
         int index = hash(str, 10);
@@ -25,15 +50,23 @@ int main()
             for (int i=0;i<size;i++){
                 hashtable[index].str[i] = str[i];
             }
+        }else{
+            append(&hashtable[index], str);
         }
-        // ....
     }
-    // test..
-    char name[3];
-    scanf("%s", name);
-    insert(hashtable, name);
-    for (int i=0;i<10;i++){
-        printf("%d -> %s\n", i, hashtable[i].str);
+    // create a hashtable
+    node *hashtable = malloc(sizeof(node)*10); // allocate ten slots in memory of size node
+    // insert three names in the hashtable
+    for (int i=0; i<3; i++){
+        char temp[10];
+        scanf("%s", temp);
+        insert(hashtable, temp);
+    }
+    // print the hashtable
+    for (int i=0; i<10; i++){
+        printf("%d - ", i);
+        printList(&hashtable[i]);
+        printf("\n");
     }
     return 0;
 }
