9adb5396b89fb368e149ff6feb23b579dedd0f49
Messaoud KIHAL <66731601+MesutKihal@users.noreply.github.com>
2021-10-29 16:50:17 +0100

    Update encrypt.c

--- encrypt.c
+++ encrypt.c
@@ -1,22 +1,23 @@
 #include <stdio.h>
+#include <string.h>
 
 int main()
 {
     // Input
-    int MAXLEN;
-    scanf("%i\n", &MAXLEN);
-    char s[MAXLEN];
-    scanf("%s\n", s);
+    char s[100];
+    scanf("%s", s);
     
     // Encrypt
-    void encrypt_text(char plain_text[MAXLEN], int key){
-        char cipher_text[MAXLEN];
-        for (int i=0; i<MAXLEN; i++){
-            int ch = (int)plain_text[i] + key;
+    void encrypt_text(char plain_text[], int key){
+        int len = (int) strlen(plain_text);
+        char cipher_text[len];
+        int ch = 0;
+        for (int i=0; i<len; i++){
+            ch = (int) plain_text[i] + key;
             cipher_text[i] = (char) ch;
         }
-        for (int j=0; j<MAXLEN; j++){
-            printf("%c", cipher_text[j]);
+        for (int i=0; i<len; i++){
+            printf("%c", cipher_text[i]);
         }
     }
     encrypt_text(s, 5);
