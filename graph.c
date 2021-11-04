ab2e59450dab4807e3ccacf59c8393dd6d84e982
Messaoud KIHAL <66731601+MesutKihal@users.noreply.github.com>
2021-11-04 13:42:47 +0100

    Update graph.c

--- graph.c
+++ graph.c
@@ -4,37 +4,34 @@
 
 int main()
 {
+    // create struct vertex
     typedef struct vertex
     {
         int value;
         struct vertex *edges[5];
     }vertex;
-    
-    void addVertex(int value, struct vertex graph[]){
-        for (int i=0; i<5; i++){
-            if (graph[i].value == 0){
-                graph[i].value = value;
-                break;
+    // create graph array
+    struct vertex *graph = malloc(sizeof(vertex)*4); // allocate four slots in the memory with the size of a vertex
+    // populate the graph
+    graph[0].value = 15;
+    graph[1].value = 12;
+    graph[2].value = 10;
+    graph[3].value = 9;
+    // add edges to the nodes of the graph
+    graph[0].edges[0] = &graph[1];
+    graph[0].edges[1] = &graph[2];
+    graph[1].edges[0] = &graph[3];
+    graph[2].edges[0] = &graph[3];
+    graph[3].edges[0] = &graph[0];
+    // print the graph
+    for (int i=0; i<4; i++){
+        printf("%d", graph[i].value);
+        for (int j=0; j<5; j++){
+            if (graph[i].edges[j] != NULL){
+                printf(" -> %d", graph[i].edges[j]->value);
             }
         }
+        printf("\n");
     }
-    void addEdge(struct vertex *a, struct vertex *b){
-        for (int i=0; i<5; i++){
-            if (a->edges[i] == NULL){
-               *a->edges[i] = *b;
-               break;
-            }
-        }
-    }
-    
-    struct vertex *graph = malloc(sizeof(vertex)*5);
-    
-    addVertex(15, graph);
-    addVertex(12, graph);
-    addVertex(10, graph);
-    addVertex(9, graph);
-    addVertex(6, graph);
-    
-
     return 0;
 }
